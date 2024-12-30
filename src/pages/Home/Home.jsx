7a5a0080acf1520a8c5e75ae0e9e7e3867c10ad8
Hafeezbaig <hafeezbaig3138@gmail.com>
2024-12-30 22:49:10 +0530

    logo updated, home page updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,24 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h2>home</h2>
+    <div className='home'>
+      <div className="hero">
+      <h1>Largest <br /> Crypto Marketplace</h1>
+      <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos</p>
+      <form>
+        <input type="text" placeholder='Search crypto..' />
+        <button type="submit">Search</button>
+      </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p>24H Change</p>
+          <p>Marketcap</p>
+        </div>
+      </div>
     </div>
   )
 }
