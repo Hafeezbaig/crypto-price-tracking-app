a8144e8419dc0de7c83f0812cb938f036df5252c
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-01-15 16:12:20 +0530

    footer and responsiveness updated

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>© Copyright 2025 - A Project by Hafeez Baig</p>
+    </div>
+  )
+}
+
+export default Footer
